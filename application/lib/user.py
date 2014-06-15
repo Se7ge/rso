dd335124ea6b8ce00154df89050495e3f3a964f9
Serge <antipov.serge@gmail.com>
2014-06-16 00:08:59 +0400

    =

---
+++ application/lib/user.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from application.database import db
+from application.models import Users
+import hashlib
+
+
+class UserAuth():
+
+    @classmethod
+    def check_user(cls, login, password):
+        user = cls.__get_by_login(login)
+        if user and cls.__check_password(user.password, password):
+            return user
+        return None
+
+    @classmethod
+    def __get_by_login(cls, login):
+        user = db.session.query(Users).filter(Users.login == login).first()
+        if user:
+            return user
+        return None
+
+    @classmethod
+    def __check_password(cls, pw_hash, password):
+        return pw_hash == hashlib.md5(password).hexdigest()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        return db.session.query(Users).get(user_id)
---
+++ application/lib/utils.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from flask.ext.login import LoginManager, current_user
+from flask.ext.principal import identity_loaded, Principal, Permission, RoleNeed, UserNeed
+from application.database import db
+from application.models import Users, Roles
+from application.app import app
+
+
+def public_endpoint(function):
+    function.is_public = True
+    return function
+
+#
+# def create_config_func(module_name, config_table):
+#
+#     def _config(code):
+#         """Возвращает значение конфигурационной переменной, полученной из таблицы %module_name%_config"""
+#         #Get app_settings
+#         app_settings = dict()
+#         try:
+#             for item in db.session.query(Settings).all():
+#                 app_settings.update({item.code: item.value})
+#             # app_settings = {item.code: item.value for item in db.session.query(Settings).all()}
+#         except Exception, e:
+#             print e
+#
+#         config = getattr(g, '%s_config' % module_name, None)
+#         if not config:
+#             values = db.session.query(config_table).all()
+#             config = dict()
+#             for value in values:
+#                 config[value.code] = value.value
+#             setattr(g, '%s_config' % module_name, config)
+#         config.update(app_settings)
+#         return config.get(code, None)
+#
+#     return _config
+
+with app.app_context():
+    permissions = dict()
+    login_manager = LoginManager()
+    try:
+        roles = db.session.query(Roles).all()
+    except Exception, e:
+        print e
+        permissions['admin'] = Permission(RoleNeed('admin'))
+    else:
+        if roles:
+            for role in roles:
+                permissions[role.code] = Permission(RoleNeed(role.code))
+                permissions[role.code].description = role.description
+        else:
+            permissions['admin'] = Permission(RoleNeed('admin'))
+
+# TODO: разобратсья как покрасивше сделать
+admin_permission = permissions.get('admin')
+user_permission = permissions.get('user')
+
+
